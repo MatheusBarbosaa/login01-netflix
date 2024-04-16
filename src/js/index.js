52c91f7294653db81e172b582bc256bc3d2610cd
MatheusBarbosaa <matheussbarbosa06@gmail.com>
2024-04-16 20:32:50 -0300

    subindo projeto

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// function mudarCor() {
+//     var botao = document.getElementById('meuBotao')
+
+//     botao.style.backgroundColor = "#00cc00";
+//     botao.disabled = true;
+// }
+
+/*a.addEventListener('click', clicar)
+
+function clicar() {
+    a.innerText = 'Entrou'
+    a.style.background = 'green'
+}*/
+
+function alterarCorBotao() {
+    var botao = document.getElementById('btnlogin');
+    botao.style.backgroundColor = 'green';
+  }
